af90868f88fed9294984923ccdfc97717eff3bb3
daksh-maheshwari-ai-enggr <dakshkhasat406@gmail.com>
2026-07-27 18:03:52 +0530

    added theatre crud apis

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db.js');
-const adminRoutes = require('./routes/admin.routes.js');
+const movieRoutes = require('./routes/movies.routes.js');
 const userRoutes = require('./routes/userRoutes.js');
+const theaterRoutes = require('./routes/theater.routes.js');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
@@ -13,8 +14,9 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/admin', adminRoutes);
+app.use('/api/admin', movieRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/admin',theaterRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
